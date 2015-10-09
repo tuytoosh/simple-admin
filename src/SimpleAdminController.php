af48c5aa0313107db183dc4d4e23a566e2173cd5
Hamid Haghdoost <tuytoosh@yahoo.com>
2015-10-09 14:20:22 +0330

    Update SimpleAdminController.php
    remove auth middleware from controller

--- src/SimpleAdminController.php
+++ src/SimpleAdminController.php
@@ -4,10 +4,6 @@ use App\User;
 
 class SimpleAdminController extends Controller {
 
-    public function __construct() {
-        $this->middleware('auth');
-    }
-
     /**
      * Display a listing of the resource.
      *
